042823d5704149fd5f1e7ba68ae881642b2165cd
erreyonj <erreyon.js@gmail.com>
2024-04-16 21:06:20 -0500

    redesigned using mobile first approach

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-const toggleMenu = () => document.querySelector('.menu').classList.toggle('open')
+const toggleMenu = () => {
+    document.querySelector('#menu').classList.toggle('closed')
+    document.querySelector('#headerBurger').classList.toggle('closed')
+    document.querySelector('#headerX').classList.toggle('closed')
+}
 
 document.querySelector('#headerBurger').addEventListener('click',toggleMenu)
+document.querySelector('#headerX').addEventListener('click',toggleMenu)
